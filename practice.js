eb8e209dfffc27b7c5fbf78f8466cd7760f2d8a4
mahywr <mahywr.yt@gmail.com>
2023-02-02 16:37:57 +0330

    commit 2

--- practice.js
+++ practice.js
@@ -5,3 +5,7 @@ const lastName = "Lovelace";
 // Only change code below this line
 const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
 console.log (lastLetterOfLastName) 
+console.log(lastName)
+a = 12
+b = 18 
+console.log (a+b)
