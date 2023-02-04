5c016a6e9fecf78e55be6a94b3120fcf14a4952d
mahywr <mahywr.yt@gmail.com>
2023-02-04 19:29:00 +0330

    array pop and psuh

--- practice.js
+++ practice.js
@@ -10,15 +10,20 @@
 // b = 18 
 // console.log (a+b)
 
-
-const arr = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [[10, 11, 12], 13, 14]
-  ];
+// // arrays
+// const arr = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [[10, 11, 12], 13, 14]
+//   ];
   
-  const subarray = arr[3];
-  const nestedSubarray = arr[3][0];
-  const element = arr[3][0][1];
-  console.log (element)
+//   const subarray = arr[3];
+//   const nestedSubarray = arr[3][0];
+//   const element = arr[3][0][1];
+//   console.log (element)
+
+const threeArr = [1, 4, 6];
+const oneDown = threeArr.pop(1);
+console.log(oneDown);
+console.log(threeArr);
