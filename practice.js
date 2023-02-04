78ff09b97cfc0adb9d046ac9f2c64ba85787006d
mahywr <mahywr.yt@gmail.com>
2023-02-04 19:22:31 +0330

    array added

--- practice.js
+++ practice.js
@@ -1,11 +1,24 @@
 
 // Setup
-const lastName = "Lovelace";
+// const lastName = "Lovelace";
 
 // Only change code below this line
-const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
-console.log (lastLetterOfLastName) 
-console.log(lastName)
-a = 12
-b = 18 
-console.log (a+b)
+// const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
+// console.log (lastLetterOfLastName) 
+// console.log(lastName)
+// a = 12
+// b = 18 
+// console.log (a+b)
+
+
+const arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14]
+  ];
+  
+  const subarray = arr[3];
+  const nestedSubarray = arr[3][0];
+  const element = arr[3][0][1];
+  console.log (element)
